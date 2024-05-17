f4265461914631b00cb9b53e4d739e526c434613
Al3xGonzales <alexandrugonzales@csu.fullerton.edu>
2024-05-17 15:04:37 -0700

    Upload Portfolio

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,19 +1,19 @@
 export const ProjectList = [
   {
     name: "CPSC 349 Project 1",
-    image: "/img/proj1.png",
+    image: "https://i.imgur.com/vCsvIRz.png",
     desc: "An interactive Quiz application which allows users to log-in, select a quiz and then view their saved grades in the gradebook. This project employed HTML, Javascript and CSS",
     link: "/projectzips/project1.zip",
   },
   {
     name: "CPSC 223 Final Project",
-    image: "/img/proj2.png",
+    image: "https://i.imgur.com/H2ZA6n8.png",
     desc: "A relatively barebones python script demonstrating the use of multi-threading and modules in python, reliant on a number of self-made and foreign modules.",
     link: "/projectzips/project2.zip"
   },
   {
     name: "CPSC 332 Final Project",
-    image: "/img/proj3.png",
+    image: "https://i.imgur.com/CBK1gRk.png",
     desc: "A functioning warehouse database, with an HTML frontend and SQL backend this project allows users to modify the database using PHP forms and imbedded SQL and Javascript. This project involved HTML, CSS, Javascript, PHP and MySql knowledge",
     link: "/projectzips/project3.zip",
   },
